253662bfdf86e3d82c172fdb17c98dfd73e00ffc
Tarun <tarunkumarsharma38@gmail.com>
2026-03-15 01:14:18 +0530

    added raycast to check the status of player is it on ground or not

--- Assets/Pixel Adventure - Asset Pack/Scripts/Player Controller.cs
+++ Assets/Pixel Adventure - Asset Pack/Scripts/Player Controller.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    
     private Rigidbody2D rb;
     private Animator anim;
-   [SerializeField] private bool isRuning;
 
+    [Header("Player Movement")] 
+   [SerializeField] private float speed;
+   [SerializeField] private float jumpForce;
+
+    [Header("Collision Check")]
+    [SerializeField]private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -23,6 +30,7 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        CheckCollision();
         Inputhandling();
         PlayerAnimation();
     }
@@ -31,17 +39,42 @@ public class PlayerController : MonoBehaviour
     {
         float Xinput = Input.GetAxisRaw("Horizontal");
         PlayerMovement(Xinput);
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+            Debug.Log("space is pressed");
+            PlayerJump();
+        }
        
     }
 
     private void PlayerMovement(float xinput)
     {
         rb.velocity= new Vector2(xinput * speed, rb.velocity.y);
+
+    }
+    private void PlayerJump()
+    {
+        Debug.Log("Player Jumped");
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
-   private void PlayerAnimation()
+    private void PlayerAnimation()
     {
 
         anim.SetFloat("Xvelocity", rb.velocity.x);
     }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(this.transform.position,Vector2.down,groundCheckDistance,groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = isGrounded ? Color.green : Color.red;
+        Gizmos.DrawRay(this.transform.position, Vector2.down * groundCheckDistance);
+
+    }
+
 }
+
