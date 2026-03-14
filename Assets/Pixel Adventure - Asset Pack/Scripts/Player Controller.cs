43164b918afd067c95f13e2bbc764f70e7d490e4
Tarun <tarunkumarsharma38@gmail.com>
2026-03-14 21:45:47 +0530

    added the animation of the player for the idle and movement state of the player

--- Assets/Pixel Adventure - Asset Pack/Scripts/Player Controller.cs
+++ Assets/Pixel Adventure - Asset Pack/Scripts/Player Controller.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rb;
+    private Animator anim;
+   [SerializeField] private bool isRuning;
 
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        anim=  this.GetComponentInChildren<Animator>();
     }
     void Start()
     {
@@ -20,17 +23,25 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        Inputhandling(); 
+        Inputhandling();
+        PlayerAnimation();
     }
 
     private void Inputhandling()
     {
         float Xinput = Input.GetAxisRaw("Horizontal");
         PlayerMovement(Xinput);
+       
     }
 
     private void PlayerMovement(float xinput)
     {
         rb.velocity= new Vector2(xinput * speed, rb.velocity.y);
+    }
+
+   private void PlayerAnimation()
+    {
+        isRuning = rb.velocity.x != 0;
+        anim.SetBool("isRuning", isRuning);
     }
 }
